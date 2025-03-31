fb5efda9583d22bfc7d8761ba65a5cd1cb022254
Rafael <rafaelnakaya@gmail.com>
2025-03-31 09:13:55 -0300

    Tipos de váriaveis

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,19 @@ console.log(nome1)
 // declarano uma varíavel nula
 let nome2=null;
 console.log(nome2)
+
+//tipos de váriavel 
+let exemplo1 = 10;
+console.log(typeof exemplo1)
+
+let exemplo2 = "Tecnologia";
+console.log(typeof exemplo2)
+
+let exemplo3 = true;
+console.log(typeof exemplo3)
+
+let exemplo4 = {};
+console.log(typeof exemplo4)
+
+let exemplo5 = [];
+console.log(typeof exemplo5)
