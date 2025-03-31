61a7d85ae031b7f6f4695fb765df7b15d761dbbf
Rafael <rafaelnakaya@gmail.com>
2025-03-31 09:54:24 -0300

    Operadores aritmétcos, Lógicos e Comparação

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,83 @@
-var nome = "fiap"
-console.log (nome)
+// var nome = "fiap"
+// console.log (nome)
 
-let idade = 19;
-console.log(idade)
+// let idade = 19;
+// console.log(idade)
 
-const sobrenome = "fiapinho"
-console.log(sobrenome)
+// const sobrenome = "fiapinho"
+// console.log(sobrenome)
 
-// declarando uma varíavel indefinida
-let nome1 
-console.log(nome1)
+// // declarando uma varíavel indefinida
+// let nome1 
+// console.log(nome1)
 
-// declarano uma varíavel nula
-let nome2=null;
-console.log(nome2)
+// // declarano uma varíavel nula
+// let nome2=null;
+// console.log(nome2)
 
-//tipos de váriavel 
-let exemplo1 = 10;
-console.log(typeof exemplo1)
+// //tipos de váriavel 
+// let exemplo1 = 10;
+// console.log(typeof exemplo1)
 
-let exemplo2 = "Tecnologia";
-console.log(typeof exemplo2)
+// let exemplo2 = "Tecnologia";
+// console.log(typeof exemplo2)
 
-let exemplo3 = true;
-console.log(typeof exemplo3)
+// let exemplo3 = true;
+// console.log(typeof exemplo3)
 
-let exemplo4 = {};
-console.log(typeof exemplo4)
+// let exemplo4 = {};
+// console.log(typeof exemplo4)
 
-let exemplo5 = [];
-console.log(typeof exemplo5)
+// let exemplo5 = [];
+// console.log(typeof exemplo5)
 
-//conversões 
+// //conversões 
 
-let numfloat =123.456;
-console.log(parseInt (numfloat))
+// let numfloat =123.456;
+// console.log(parseInt (numfloat))
 
-let numstring = "123.456";
-console.log(parseFloat(numstring))
+// let numstring = "123.456";
+// console.log(parseFloat(numstring))
 
-// Métodos
+// // Métodos
 
-//leght =  verifica o tamanho da string
-let frase = "O mundo da tecnologia";
-console.log(frase.length)
+// //leght =  verifica o tamanho da string
+// let frase = "O mundo da tecnologia";
+// console.log(frase.length)
 
-//indexOf -  ele retorna um trecho do texto
-let texto ="A programação indomável e sustentável";
-console.log(texto.indexOf(""))
+// //indexOf -  ele retorna um trecho do texto
+// let texto ="A programação indomável e sustentável";
+// console.log(texto.indexOf(""))
 
-//slice - retorna um trecho o texto com inicio e final
-let info = "Processamento de ponta";
-console.log(info.slice(16, 25))
+// //slice - retorna um trecho o texto com inicio e final
+// let info = "Processamento de ponta";
+// console.log(info.slice(16, 25))
 
+// //Opeadores Aritméticos
+// const num1 = 10;
+// const num2 = 20;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// Operadores Lógicos 
+
+const num3 = 20;
+const num4 = "20";
+
+console.log(num3 < num4);
+console.log(num3 > num4 && num4 < num3);
+console.log(num3 < num4 || num4 < num3);
+
+//Operadores de Comparação
+// "==" compara somente valores e "===" compara valores e tipos
+console.log(num3 == num4);
+console.log(num3 === num4)
+
+// //reverter palavras
+// let palavra = prompt("escreva algo")
+// let palavraInvertida = palavra.split("").reverse("").join("")
+// console.log(`a sua palavra foi: ${palavra}`)
+// console.log (`a palavra revertida ficou: ${palavraInvertida}`) 
