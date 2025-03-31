7c4b5d8a613a707174ebcde6448193296840aa58
Rafael <rafaelnakaya@gmail.com>
2025-03-31 09:08:11 -0300

    Declarações de Váriaveis

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+var nome = "fiap"
+console.log (nome)
+
+let idade = 19;
+console.log(idade)
+
+const sobrenome = "fiapinho"
+console.log(sobrenome)
+
+// declarando uma varíavel indefinida
+let nome1 
+console.log(nome1)
+
+// declarano uma varíavel nula
+let nome2=null;
+console.log(nome2)
