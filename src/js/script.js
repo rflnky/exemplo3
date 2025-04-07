91595eb69ffb9f62f7e62f8f1aa33291b7c26a20
Rafael <rafaelnakaya@gmail.com>
2025-04-07 09:15:39 -0300

    Criando condicionais com If; If/Else e Switch

--- src/js/script.js
+++ src/js/script.js
@@ -1,83 +1,133 @@
-// var nome = "fiap"
-// console.log (nome)
+// //DECLARARAÇÕES E VARIAVEIS
+
+// var nome ="fiap";
+// console.log(nome);
 
 // let idade = 19;
 // console.log(idade)
 
-// const sobrenome = "fiapinho"
+// const sobrenome="Fiapinho";
 // console.log(sobrenome)
 
-// // declarando uma varíavel indefinida
-// let nome1 
+// //declarando uma variavel indefinida
+// let nome1;
 // console.log(nome1)
 
-// // declarano uma varíavel nula
+// //declarando uma variavel nula
 // let nome2=null;
 // console.log(nome2)
 
-// //tipos de váriavel 
-// let exemplo1 = 10;
+// //TIPOS DE VARIAVEIS
+
+// let exemplo1 =10;
 // console.log(typeof exemplo1)
 
-// let exemplo2 = "Tecnologia";
+// let exemplo2 ="Tecnologia";
 // console.log(typeof exemplo2)
 
-// let exemplo3 = true;
+// let exemplo3 =true;
 // console.log(typeof exemplo3)
 
-// let exemplo4 = {};
+// let exemplo4={};
 // console.log(typeof exemplo4)
 
-// let exemplo5 = [];
+// let exemplo5 =[];
 // console.log(typeof exemplo5)
 
-// //conversões 
+// //CONVERSÕES
 
-// let numfloat =123.456;
-// console.log(parseInt (numfloat))
+// let numFloat =123.456;
+// console.log(parseInt(numFloat));
 
-// let numstring = "123.456";
-// console.log(parseFloat(numstring))
+// let numString ="123.456";
+// console.log(parseFloat(numString));
 
-// // Métodos
+// //METODOS
 
-// //leght =  verifica o tamanho da string
-// let frase = "O mundo da tecnologia";
-// console.log(frase.length)
+// //length = verifica o tamanho da string
+// let frase ="O mundo da tecnologia";
+// console.log(frase.length);
 
-// //indexOf -  ele retorna um trecho do texto
-// let texto ="A programação indomável e sustentável";
+// //indexOf -retorna um trecho do texto
+
+// let texto="A programação indomável e sustentavel";
 // console.log(texto.indexOf(""))
 
-// //slice - retorna um trecho o texto com inicio e final
-// let info = "Processamento de ponta";
-// console.log(info.slice(16, 25))
 
-// //Opeadores Aritméticos
-// const num1 = 10;
-// const num2 = 20;
+// //slice - retorna um trecho do texto com inicio e final
+
+// let info ="Processamento de ponta";
+// console.log(info.slice(16,25))
+
+//OPERADORES ARITIMÉTICOS
+
+// const num1=10;
+// const num2=20;
 
 // console.log(num1 + num2);
 // console.log(num1 - num2);
 // console.log(num1 * num2);
 // console.log(num1 / num2);
 
-// Operadores Lógicos 
+// OPERADORES LÓGICOS
 
-const num3 = 20;
-const num4 = "20";
+// const num3=20;
+// const num4="30";
 
-console.log(num3 < num4);
-console.log(num3 > num4 && num4 < num3);
-console.log(num3 < num4 || num4 < num3);
+// console.log(num3 < num4);
+// console.log(num3 > num4 && num4 < num3);
+// console.log(num3 < num4 || num4 < num3); 
 
-//Operadores de Comparação
-// "==" compara somente valores e "===" compara valores e tipos
-console.log(num3 == num4);
-console.log(num3 === num4)
+// //OPERADORES DE COMPARAÇÃO
 
-// //reverter palavras
-// let palavra = prompt("escreva algo")
-// let palavraInvertida = palavra.split("").reverse("").join("")
-// console.log(`a sua palavra foi: ${palavra}`)
-// console.log (`a palavra revertida ficou: ${palavraInvertida}`) 
+// console.log(num3 == num4);
+// console.log(num3 === num4);
+
+//Estrutura Condicional
+
+//if
+let logado="fiap"
+
+if(logado=="fiap"){
+    console.log("é verdadeiro")
+}
+
+//if/else
+
+let usuário="teste"
+if(usuário=="teste"){
+    console.log("usuário correto")
+}else{
+    console.log("usuário incorreto")    
+}
+
+let idade =15
+
+if(idade < 15){
+    console.log("Menor de Idade")
+}else if(idade > 15 && idade < 18){
+    console.log("Não é maior de idade, mas pode entrar")
+}else{
+    console.log("Pode entrar, é maior de idade")
+}
+
+//switch
+
+let time = "Corithians"
+
+switch(time){
+    case "Corithians":
+        console.log("Pior time")
+        break;
+    case "Sao Paulo":
+        console.log("3 mundiais")
+        break;
+    case "Palmeiras":
+        console.log("Não tem Munidal")
+        break;
+    default:
+        console.log("Nenhuma das Opções")
+}
+
+//ternário
+
